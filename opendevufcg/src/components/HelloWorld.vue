e0f7764950781298310c0e29566fa7275cd26dc0
Juan Barros <juan.barros@ccc.ufcg.edu.br>
2019-02-16 11:16:04 -0300

    Cria projeto base em Vue.js

---
+++ opendevufcg/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    OpenDevUFCG
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
