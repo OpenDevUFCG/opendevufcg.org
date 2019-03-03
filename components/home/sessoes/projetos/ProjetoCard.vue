5dcc0fb61faac073d5fc64a8f8ad2af07bbd136e
Thayanne <thayannevls@gmail.com>
2019-03-02 21:31:49 -0300

    cria componente de projetos

---
+++ components/home/sessoes/projetos/ProjetoCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="card">
+    <img :src="projeto.imagem" style="width:100%">
+    <a class="titulo" :href="projeto.site" target="_blank"> 
+      {{ projeto.titulo }}
+    </a>
+    <p class="descricao">
+      {{ projeto.descricao }}
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProjetoCard',
+  props: {
+    projeto: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.card {
+  width: 300px;
+  max-height: 100vh;
+  height: 410px;
+  text-align: center;
+  padding: 0px 20px;
+  padding-bottom: 40px;
+  border-radius: 4px;
+  box-shadow: 0 15px 35px rgba(50, 50, 90, 0.1), 0 5px 15px rgba(0, 0, 0, 0.07);
+  border: 1px solid #d0d0d0;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+  cursor: default;
+}
+
+.card:hover {
+  box-shadow: 0 18px 35px rgba(50, 50, 90, 0.1), 0 8px 15px rgba(0, 0, 0, 0.07);
+  transform: translateY(-1px);
+}
+
+.titulo {
+  display: inline-block;
+  text-decoration: none;
+  letter-spacing: 2px;
+  color: #35495e;
+  margin-bottom: 20px;
+  cursor: pointer;
+  font-size: 30px;
+}
+
+.titulo:hover {
+  text-decoration: underline;
+}
+
+.descricao {
+  text-align: center;
+  color: #80858a;
+}
+
+@media (max-width: 600px) {
+  .card {
+    width: 95%;
+    margin: 0px 5px;
+  }
+}
+</style>
---
+++ components/home/sessoes/projetos/Projetos.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="section--allscreen">
+    <div class="projetosGrid">
+      <projeto-card v-for="projeto in projetos" :key="projeto.site" :projeto="projeto" />
+    </div>
+  </section>
+</template>
+<script>
+import ProjetoCard from './ProjetoCard.vue'
+export default {
+  name: 'Projetos',
+  components: {
+    ProjetoCard
+  },
+  data() {
+    return {
+      projetos: [
+        {
+          titulo: 'Glossário UFCG',
+          descricao:
+            'Glossário contendo significados de siglas e gírias usadas por alunos da UFCG.',
+          site: 'https://glossario.opendevufcg.org/',
+          imagem: '/opendev_icon.png'
+        },
+        {
+          titulo: 'Tamburetei',
+          descricao:
+            'Repositório contendo materiais para ajudar alunos fazerem de tamburete cadeiras de Computação@UFCG.',
+          site: 'https://github.com/opendevufcg/Tamburetei/',
+          imagem: '/opendev_icon.png'
+        },
+        {
+          titulo: 'IssueAi',
+          descricao:
+            'O Issue Ai cria um espaço de visibilidade para os projetos open source de Computação@UFCG.',
+          site: 'https://issueai.opendevufcg.org/',
+          imagem: '/opendev_icon.png'
+        },
+        {
+          titulo: 'Roadmap CC',
+          descricao: 'Roadmap para se tornar um cientista de Computação@UFCG.',
+          site: 'https://github.com/OpenDevUFCG/roadmap-cc',
+          imagem: '/opendev_icon.png'
+        },
+        {
+          titulo: 'Laguinho API',
+          descricao:
+            'Onde todos os dados, pessoas e radiação da UFCG se encontram.',
+          site: 'https://github.com/OpenDevUFCG/laguinho-api',
+          imagem: '/opendev_icon.png'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.projetosGrid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+
+  * {
+    margin: 10px;
+  }
+}
+
+@media (max-width: 600px) {
+  .projetosGrid {
+    flex-direction: column;
+  }
+}
+</style>
