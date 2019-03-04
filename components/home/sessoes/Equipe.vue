b90733fdb6304fc180f8b4aad8130f2e35f325eb
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2019-03-04 11:27:42 -0300

    Add team data

--- components/home/sessoes/Equipe.vue
+++ components/home/sessoes/Equipe.vue
@@ -5,7 +5,58 @@
 </template>
 <script>
 export default {
-  name: 'Equipe'
+  name: 'Equipe',
+  data() {
+    return {
+      equipe = [
+        {
+          nome: 'Fanny Vieira',
+          url_github: 'https://github.com/FannyVieira',
+          url_img: 'https://avatars3.githubusercontent.com/u/14113480?s=460&v=4',
+        },
+        {
+          nome: 'Héricles Emanuel',
+          url_github: 'https://github.com/hericlesme',
+          url_img: 'https://avatars1.githubusercontent.com/u/30700596?s=460&v=4',
+        },
+        {
+          nome: 'José Renan',
+          url_github: 'https://github.com/JoseRenan',
+          url_img: 'https://avatars3.githubusercontent.com/u/11728746?s=460&v=4',
+        },
+        {
+          nome: 'José Robson',
+          url_github: 'https://github.com/JRobsonJr',
+          url_img: 'https://avatars1.githubusercontent.com/u/20714148?s=460&v=4',
+        },
+        {
+          nome: 'Juan Barros',
+          url_github: 'https://github.com/JuanBarros2',
+          url_img: 'https://avatars0.githubusercontent.com/u/11728655?s=460&v=4',
+        },
+        {
+          nome: 'Julio Guedes',
+          url_github: 'https://github.com/juliobguedes',
+          url_img: 'https://avatars2.githubusercontent.com/u/23748073?s=460&v=4',
+        },
+        {
+          nome: 'Lucas Medeiros',
+          url_github: 'https://github.com/LukeHxH',
+          url_img: 'https://avatars0.githubusercontent.com/u/19390820?s=460&v=4',
+        },
+        {
+          nome: 'Matheus Alves',
+          url_github: 'https://github.com/SpinnelSun',
+          url_img: 'https://avatars0.githubusercontent.com/u/30247519?s=460&v=4',
+        },
+        {
+          nome: 'Thayanne Luiza',
+          url_github: 'https://github.com/thayannevls',
+          url_img: 'https://avatars0.githubusercontent.com/u/6867958?s=460&v=4',
+        }
+      ]
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
