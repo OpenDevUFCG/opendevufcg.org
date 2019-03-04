f8dc707bc7142cf0b9a0d869290b665923093ce6
Juan Barros <juan.barros@ccc.ufcg.edu.br>
2019-03-04 15:55:37 -0300

    Adiciona hexagono em itens

--- components/home/sessoes/Visao.vue
+++ components/home/sessoes/Visao.vue
@@ -6,11 +6,11 @@
       para projetos de software, especialmente quando eles tem potencial
       de atingir várias pessoas. Buscamos criar um ambiente open source para:
       <ul>
-        <li>estimular a interação entre estudantes;</li>
-        <li>estimular a participação em repositórios abertos;</li>
-        <li>prover mecanismos para a contribuição por estudantes desde os primeiros períodos</li>
-        <li>desenvolver projetos de/para estudantes</li>
-        <li>impulsionar projetos open source</li>
+        <li>Estimular a interação entre estudantes;</li>
+        <li>Estimular a participação em repositórios abertos;</li>
+        <li>Prover mecanismos para a contribuição por estudantes desde os primeiros períodos</li>
+        <li>Desenvolver projetos de/para estudantes;</li>
+        <li>Impulsionar projetos open source;</li>
       </ul>
     </p>
   </section>
@@ -20,14 +20,42 @@ export default {
   name: 'Visao'
 }
 </script>
-<style scoped>
+<style lang="scss" scoped>
+section {
+  margin: 0px;
+}
+
 ul {
-  margin-top: 10px;
+  margin: 10px 0 0 0;
+  padding: 0 0 0 6px;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
 }
 li {
   margin-top: 3px;
+  margin: 0;
+  padding: 15px 0 15px 30px;
+  list-style: none;
+  background-repeat: no-repeat;
+  background-position: left center;
+  background-size: 24px;
+  background-image: url('~static/hexagon.svg');
+  text-align: initial;
+  &:nth-of-type(2n) {
+    background-position: right center;
+    text-align: right;
+    padding: 15px 30px 15px 0px;
+  }
 }
 p {
-  margin-top: 20px;
+  text-align: center;
+  font-size: 1.3rem;
+  word-wrap: break-word;
+}
+@media (max-width: 790px) {
+  section {
+    margin: 20px 14px 0 14px;
+  }
 }
 </style>
