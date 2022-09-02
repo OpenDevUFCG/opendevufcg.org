d3b68f5e5fb8e1ace302c3807aeed4126cf0da69
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2022-09-02 15:18:15 -0300

    fix: Retirando nomes de classes pois está sendo utilizando Styled Components

--- src/pages/AboutSection/index.js
+++ src/pages/AboutSection/index.js
@@ -7,22 +7,22 @@ export const AboutSection = () => {
     <Wrapper>
           <Title>Sobre nós</Title>
           <p>Somos uma comunidade voltada a incentivar a cultura open source no curso de Ciência da Computação da UFCG</p>
-          <CardsContainer class="about-us-container">
-            <Card class="about-us-card">
+          <CardsContainer>
+            <Card>
               <div>
                 <img src={ComunnityImg} alt="Representação de rrês pessoas formando uma corrente para representar uma comunidade."/>
               </div>
               <HighlightText>Comunidade</HighlightText>
               <p>Juntos fazemos mais, por isso nos esforçamos em criar uma comunidade que exista diálogo, amizade, respeito e suporte. Todos são bem vindos, seja para contribuir, sugerir novas ideias ou pedir ajuda.</p>
             </Card>
-            <Card class="about-us-card">
+            <Card>
               <div>
                 <img src={LightBoxImg} alt="Representação de uma caixa de papelão com uma lâmpada saindo através do topo dela, a lâmpada está acesa"/>
               </div>
               <HighlightText>Open Source</HighlightText>
               <p>Open Source permite que qualquer pessoa possa ver, modificar, contribuir e distribuir seu código. Adotamos o Open Source como filosofia pois acreditamos que é isso que nos conecta, todos nossos projetos são Open Source e queremos espalhar essa cultura pela UFCG e quem sabe até fora dela.</p>
             </Card>
-            <Card class="about-us-card">
+            <Card>
               <div>
                 <img src={EmpowermentImg} alt="Representação de uma pessoa dançando, com uma estrela, serpentina, quadrado e nota musical ao lado."/>
               </div>
