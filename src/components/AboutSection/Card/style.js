c5c27d9a2b5d3ffa34a427ece4254ee2369f4f02
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2022-09-02 16:00:37 -0300

    fix: Separando as responsabilidades e alterando mudanças no estilo

---
+++ src/components/AboutSection/Card/style.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    flex-direction: column;
+   
+
+    p {
+        word-wrap: break-word;
+        text-align: center;
+    }
+
+    & > div {
+        display: flex;
+        justify-content: center;
+        height: 135px;
+    }
+
+    @media screen and (max-width:768px) {
+        margin: 2rem;   
+    }
+`
+export const Icon = styled.img`
+ 
+    height: 6.875rem;
+    
+   
+`
+
+export const HighlightText = styled.p`
+    font-weight: bold;
+    font-size: 1.75rem;
+    margin-top: 2rem;
+    margin-bottom: 1rem;
+`
---
+++ src/components/AboutSection/style.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+    padding: 0 3rem;
+    background:  linear-gradient(180deg, #4979B7 0%, #3C457B 65.85%, #393A6E 100%), linear-gradient(180deg, #4979B7 0%, #393A6E 100%);
+    border-radius: 25px;
+    color: #FFF;
+ 
+    & > p {
+        font-size: 1.5rem;
+        text-align: center;
+    }
+
+    @media screen and (max-width:768px) {
+        max-height: none;
+    }
+`;
+
+export const Title = styled.h2`
+    text-align: center;
+    font-size: 2rem;
+    margin-bottom: 1rem;
+    text-transform: uppercase;
+`
+export const CardsContainer = styled.div`
+    display: flex;
+    gap: 1.5rem;
+    margin: 7.5rem 0;
+
+
+    @media screen and (max-width:768px) {
+        flex-direction: column;
+        padding: 1rem;
+        margin-top: 1rem;
+        align-items: center;
+    }
+`
