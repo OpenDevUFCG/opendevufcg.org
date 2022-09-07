37d7a402f9a9fab78585a883dbd2566881788487
leandra.silva <leandra.silva@ccc.ufcg.edu.br>
2022-09-07 03:32:30 -0300

    adiciona responsividade

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -13,14 +13,21 @@ export const Links = styled.ul`
   font-weight: bold;
   font-size: 22px;
   text-transform: uppercase;
+  cursor: pointer;
 
-  @media screen and (max-width: 800px) {
-    font-size: 9px;
-    gap: 0.8rem;
+  @media screen and (max-width: 480px) {
+    font-size: 10px;
+    gap: 0.5rem;
   }
-  @media screen and (min-width: 1024px) {
-    font-size: 14px;
-    gap: 2rem;
+
+  @media screen and (max-width: 375px) {
+    font-size: 9px;
+    gap: 0.4rem;
+  }
+
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+    gap: 1.5rem;
   }
 
   li {
@@ -35,20 +42,19 @@ export const Links = styled.ul`
         position: absolute;
         width: 100%;
         height: 3px;
-        top: 2rem;
+        top: 1.8rem;
         background-color: #343578;
         transform: scaleX(0);
         transform-origin: top left;
         transition: transform 0.5s ease;
 
-        @media screen and (max-width: 700px) {
+        @media screen and (max-width: 480px) {
           top: 1rem;
           height: 2px;
         }
 
-        @media screen and (min-width: 1024px) {
+        @media (min-width: 768px) {
           top: 1.5rem;
-          height: 3px;
         }
       }
 
