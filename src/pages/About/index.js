dea163349a921010507d7c2c766f02278f4d7b39
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-09-07 17:45:50 -0300

    REmove componente carousel da pagina about

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,11 +1,9 @@
-import { Carousel } from "../../components/Carousel";
 import { AboutSection } from "../../components/AboutSection";
 import { Wrapper } from "./style";
 
 export const About = () => {
   return (
     <Wrapper>
-      <Carousel />
       <AboutSection />
     </Wrapper>
   );
