a8382644ec66481cf53356703b6024c9f15d3f77
João Lima <jpalvesl101@gmail.com>
2022-09-09 14:16:52 +0000

    fazendo alterações sugeridas no PR

--- src/components/Socials/styles.js
+++ src/components/Socials/styles.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Card = styled.a`
   display: flex;
-  width: 549px;
-  height: 216px;
+  align-items: center;
+  max-width: 35rem;
   background-color: #F9F9F9;
   color: inherit;
   text-decoration: none;
@@ -13,8 +13,7 @@ export const Card = styled.a`
 
   @media (max-width: 600px) {
     flex-direction: column;
-    width: 216px;
-    height: 480px;
+    width: 18rem;
   }
 `
 
@@ -27,17 +26,17 @@ export const CardText = styled.div`
 
 export const Title = styled.h2`
   font-size: 24px;
-  margin-top: 30px;
+  margin-top: 1rem;
   color: #343587;
 `
 
 export const Description = styled.p`
-  margin-top: 41px;
+  margin: 2rem 0;
   font-size: 16px;
   letter-spacing: 0.125rem;
 `
 
 export const Icon = styled.img`
-  width: 13.5rem;
+  width: 100%;
   order: 1;
 `
