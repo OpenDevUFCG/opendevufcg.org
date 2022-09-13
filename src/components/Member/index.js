6fb2a0745a93b2ecd442bf2d65c65116a9f23891
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2022-09-13 16:57:43 -0300

    refactor: Mudando nome da prop para ficar mais generalista

--- src/components/Member/index.js
+++ src/components/Member/index.js
@@ -1,8 +1,16 @@
 import { Wrapper, Tooltip } from "./style"
 
-export const Member = ({ githubURL, srcImg, name, widthInRem}) => {
+/**
+ * 
+ * @githubURL {string} Uma url do Github do membro
+ * @srcImg {string} O source da imagem do membro
+ * @name {string} O nome do membro
+ * @width {number} Um número que será a medida do componente, o componente será renderizado com a unidade em rem.
+ *  
+ */
+export const Member = ({ githubURL, srcImg, name, width}) => {
     return(
-        <Wrapper href={githubURL} target="_blank" width={widthInRem}>
+        <Wrapper href={githubURL} target="_blank" width={width}>
             <div>
                 <img src={srcImg} alt={`Foto de ${name}`}/>
             </div>
