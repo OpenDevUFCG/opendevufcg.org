72f0d97eb62866eac4fe3f1d0ad5261c6de742bd
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2022-09-14 10:57:55 -0300

    refactor: moving SectionTitle to another folder

--- src/components/SectionTitle/index.js
+++
@@ -1,9 +0,0 @@
-import { Title } from "./style";
-
-export const SectionTitle = ({title}) => {
-  return (
-    <Title>
-      {title}
-    </Title>
-  )
-};
---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const SectionTitle = styled.h2`
+    color: #393A6E;
+    font-weight: 700;
+    font-size: 2.25rem;
+    text-transform: uppercase;
+    
+    @media screen and (max-width:768px) {
+        font-size: 2rem;
+    }
+`
